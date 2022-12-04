9d0536744e485bb8a41177b9bac996cb88a1dd79
Kimball Leavitt <k@httpstat.net>
2022-12-03 23:09:14 -0700

    added combSort

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -160,6 +160,28 @@ function shellSort(arr) {
     return arr;
 }
 
+function combSort(arr) {
+    let gap = arr.length;
+    shrink = 1.3;
+    sorted = false;
+
+    while (!sorted) {
+        gap = Math.floor(gap / shrink);
+        if (gap <= 1) {
+            gap = 1;
+            sorted = true;
+        }
+        for (let i = 0; i + gap < arr.length; i++) {
+            if (arr[i] > arr[i + gap]) {
+                swap(arr, i, i + gap);
+                sorted = false;
+            }
+        }
+    }
+
+    return arr;
+}
+
 // TEST CASES
 
 
@@ -185,6 +207,7 @@ function runTests() {
         quickSort,
         bubbleSort,
         shellSort,
+        combSort,
     ];
 
     sortFunctions.forEach(function(sort) {
